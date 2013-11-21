681466c6464130d35b695cf7c4ab8fb50dfd4db9
madhulikagurav <madhulikagurav@gmail.com>
2013-11-21 20:41:39 +1200

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,53 @@
+
+            // When the document is ready
+    $(document).ready(function () {
+                
+          
+               //validation rules
+                //by default it will append a <label class="error> element to the invalid input
+                //and will add a "error" class to the input
+
+       $("#example1").validate({
+                    rules: {
+                        username:
+				 {
+                            		required: true,
+                            		email: true
+                        	 },  
+                        password:
+				 {
+                            		required: true,
+                            		minlength: 5
+			
+                        	} 
+                   	    },
+		        // Specify the validation error messages
+
+        	   messages:
+	 		{
+		
+            password:
+				 {
+
+			                required: "Please provide a password",
+			
+                minlength: "Your password must be at least 5 characters long"
+		
+                 },
+
+		
+            email: "Please Enter a valid E-mail address"
+		            
+		
+        },
+        
+
+		
+
+                });
+            });
+      
+
+
+
+
